4fc43d317442af8459a188881780b29f11db1a1d
Qaisar Baltabayev <q.baltabayev@icloud.com>
2023-11-08 23:45:31 +0600

    test for Authenticate and CreditTransaction API

--- src/test/java/org/example/controller/CreditTransactionControllerTest.java
+++ src/test/java/org/example/controller/CreditTransactionControllerTest.java
@@ -1,85 +1,87 @@
 package org.example.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.controller.CreditTransactionController;
+import org.example.core.domain.Player;
+import org.example.core.service.WalletPlayerService;
+import org.example.core.service.WalletTransactionsService;
 import org.example.dto.transaction.TransactionWithId;
 import org.example.dto.transaction.TransactionWithoutId;
-import org.example.exception.TransactionException;
 import org.example.manager.PlayerManagerImpl;
-import org.example.wrapper.PlayerWrapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CreditTransactionControllerTest {
 
-    private CreditTransactionController creditTransactionController;
+    @Mock
     private PlayerManagerImpl playerManager;
-    private ObjectMapper objectMapper;
+
+    @InjectMocks
+    private CreditTransactionController controller;
+
+    @Mock
+    private WalletPlayerService playerService;
+
+    @Mock
+    private WalletTransactionsService transactionsService;
 
     @BeforeEach
-    public void setUp() {
-        playerManager = mock(PlayerManagerImpl.class);
-        objectMapper = new ObjectMapper();
-        creditTransactionController = new CreditTransactionController(playerManager);
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    public void testCreditTransactionWithTransactionId_Success() throws Exception {
-        TransactionWithId transaction = new TransactionWithId(123, BigDecimal.valueOf(100.00));
-        String token = "testToken";
+    public void testCreditTransactionWithTransactionId() {
+        // Create a sample TransactionWithId and token
+        TransactionWithId transaction = new TransactionWithId(1, BigDecimal.valueOf(100.0));
+        String token = "Bearer yourAuthTokenHere";
 
-        ResponseEntity<Map<String, String>> expectedResponse = ResponseEntity.ok(Map.of("message", "Credit is successful"));
-        when(playerManager.creditWithTransactionId(transaction, token)).thenReturn(expectedResponse);
+        // Mock dependencies and behavior
+        when(playerService.findByUsername(anyString()))
+                .thenReturn(Optional.of(new Player(/* create a Player object here */)));
+        when(transactionsService.findById(1))
+                .thenReturn(Optional.empty());
 
-        String requestBody = objectMapper.writeValueAsString(transaction);
-        ResponseEntity<Map<String, String>> response = creditTransactionController.creditTransactionWithTransactionId(transaction, token);
+        // Mock the playerManager method to return a ResponseEntity
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Транзакция успешно завершена");
+        when(playerManager.creditWithTransactionId(transaction, token))
+                .thenReturn(ResponseEntity.ok(response));
 
-        verify(playerManager, times(1)).creditWithTransactionId(transaction, token);
+        // Call the controller method
+        ResponseEntity<Map<String, String>> result = controller.creditTransactionWithTransactionId(transaction, token);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(expectedResponse.getBody(), response.getBody());
+        // Verify the result
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals("Транзакция успешно завершена", result.getBody().get("message"));
     }
 
     @Test
-    public void testCreditTransactionWithTransactionId_Failure() throws Exception {
-        TransactionWithId transaction = new TransactionWithId(123, BigDecimal.valueOf(100.00));
-        String token = "testToken";
+    public void testCreditTransactionWithoutTransactionId() {
+        // Create a sample TransactionWithoutId and token
+        TransactionWithoutId transactionWithoutId = new TransactionWithoutId(BigDecimal.valueOf(50.0));
+        String token = "Bearer yourAuthTokenHere";
 
-        when(playerManager.creditWithTransactionId(transaction, token)).thenThrow(new TransactionException("Transaction failed"));
+        // Mock the behavior of the random number generation
+        when(playerManager.creditWithoutTransactionId(any(), anyString()))
+                .thenCallRealMethod(); // Use the actual method implementation
 
-        String requestBody = objectMapper.writeValueAsString(transaction);
-        ResponseEntity<Map<String, String>> response = creditTransactionController.creditTransactionWithTransactionId(transaction, token);
+        // Call the controller method
+        ResponseEntity<Map<String, String>> result = controller.creditTransactionWithoutTransactionId(transactionWithoutId, token);
 
-        verify(playerManager, times(1)).creditWithTransactionId(transaction, token);
-
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals("Transaction failed", response.getBody().get("error"));
+        // Verify the result
+        assertNotNull(result);
+        // You can add more specific assertions here for your use case.
     }
-
-    @Test
-    public void testCreditTransactionWithoutTransactionId_Success() throws Exception {
-        TransactionWithoutId transaction = new TransactionWithoutId(BigDecimal.valueOf(100.00));
-        String token = "testToken";
-
-        ResponseEntity<Map<String, String>> expectedResponse = ResponseEntity.ok(Map.of("message", "Credit is successful"));
-        when(playerManager.creditWithoutTransactionId(transaction, token)).thenReturn(expectedResponse);
-
-        String requestBody = objectMapper.writeValueAsString(transaction);
-        ResponseEntity<Map<String, String>> response = creditTransactionController.creditTransactionWithoutTransactionId(transaction, token);
-
-        verify(playerManager, times(1)).creditWithoutTransactionId(transaction, token);
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(expectedResponse.getBody(), response.getBody());
-    }
-
 }
